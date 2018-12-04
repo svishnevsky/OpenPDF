8cb7e819a5b645cc9b30728b2469537e2ef88e6b
Sergey Vishnevsky <svishnevskij@gmail.com>
2018-12-04 15:23:49 +0300

    Add PDF file version reader

---
+++ src/OpenPDF/PdfReader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace OpenPDF
+{
+    public class PdfReader
+    {
+        private static readonly int versionPrefixLength = "%PDF-".Length;
+        private readonly Stream stream;
+
+        public PdfReader(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public async Task<string> ReadVersion()
+        {
+            using (var reader = new StreamReader(this.stream))
+            {
+                var versionLine = await reader.ReadLineAsync();
+                return versionLine.Substring(versionPrefixLength);
+            }
+        }
+    }
+}
---
+++ tests/OpenPDF.Tests/PdfReaderTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OpenPDF.Tests
+{
+    [TestClass]
+    public class PdfReaderTests
+    {
+        [TestMethod]
+        public async Task ReadVersion()
+        {
+            const string expected = "1.7";
+            string result;
+            using (var stream = new FileStream("example.pdf", FileMode.Open))
+            {
+                var sut = new PdfReader(stream);
+
+                result = await sut.ReadVersion();
+            }
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
